b123ddd6075119872ce5bc75d549ec87eafb44fb
Bruno <juegos.malafar@gmail.com>
2023-05-31 01:28:52 -0300

    Se reemplaza dependencia para hacer efecto typed.
    Se cambia react-typed por typewriter-effect ya que la anterior daba error al hacer build.

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Typed from "react-typed";
+import Typewriter from "typewriter-effect";
 import Button from "./Button";
 
 const Hero = () => {
@@ -12,19 +12,25 @@ const Hero = () => {
           <p className="md:text-5xl sm:text-4xl text-xl font-bold">
             Fast, flexible financing for
           </p>
-          <Typed
-            strings={["BTB", "BTC", "SASS"]}
-            typeSpeed={120}
-            backSpeed={140}
+          <Typewriter
             loop
-            className="md:text-5xl sm:text-4xl text-xl font-bold pl-2"
+            options={{
+              wrapperClassName:
+                "md:text-5xl sm:text-4xl text-xl font-bold pl-2",
+              cursorClassName: "md:text-5xl sm:text-4xl text-xl",
+              strings: ["BTB", "BTC", "SASS"],
+              autoStart: true,
+              loop: true,
+              delay: 120,
+              deleteSpeed: 140,
+            }}
           />
         </div>
         <p className="md:text-2xl text-xl font-bold text-gray-500 pt-2">
           Monitor your data analytycs to increase revenue for BTB, BTC, & SASS
           plataforms.
         </p>
-       <Button color={"green"} text={"Get Started"}/>
+        <Button color={"green"} text={"Get Started"} />
       </div>
     </div>
   );
